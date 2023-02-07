44a6912b88539ff32d295aa1d4f36c3b53d12cf7
tunchahmet <tunch.ahmet@gmail.com>
2023-02-07 16:21:31 +0300

    first commit

---
+++ src/data/navData.js
@@ -0,0 +1,16 @@
+const navData = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "About",
+    path: "/about",
+  },
+  {
+    name: "Blog",
+    path: "/blog",
+  },
+];
+
+export default navData;
