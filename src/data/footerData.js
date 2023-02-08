cef74bb5a9291d7f17b8f77d958a9dee4d5b6e52
tunchahmet <tunch.ahmet@gmail.com>
2023-02-08 23:56:15 +0300

    link fixes

--- src/data/footerData.js
+++ src/data/footerData.js
@@ -1,24 +1,24 @@
 const footerData = [
-  {
-    name: "Privacy",
-    path: "/policies/privacy",
-  },
-  {
-    name: "Terms",
-    path: "/policies/terms",
-  },
-  {
-    name: "blogAstro",
-    path: "/",
-  },
-  {
-    name: "About",
-    path: "/about",
-  },
-  {
-    name: "Blog",
-    path: "/blog",
-  },
+	{
+		name: 'Privacy',
+		path: '#',
+	},
+	{
+		name: 'Terms',
+		path: '/#',
+	},
+	{
+		name: 'blogAstro',
+		path: '/',
+	},
+	{
+		name: 'About',
+		path: '/about',
+	},
+	{
+		name: 'Blog',
+		path: '/blog',
+	},
 ];
 
 export default footerData;
