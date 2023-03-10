9295d3929ad626f088627ef9b21a7f3a20d33ab5
SouthChain Digital <ryanpark88.rp@gmail.com>
2023-03-10 21:53:31 +0000

    Update 4 files

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
 // Place any global configuration here
-export const SITE_TITLE = 'blogAstro';
-export const SITE_DESCRIPTION = 'blogAstro Description';
+export const SITE_TITLE = 'coinwire.us';
+export const SITE_DESCRIPTION = 'coinwire.us';
